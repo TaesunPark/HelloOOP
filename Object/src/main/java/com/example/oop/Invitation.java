b7adc2433d07054fcb5646b1f55e1a350824a953
taesunpark <tovbskvb@daum.net>
2022-03-24 14:32:05 +0900

    [add] Object - 조영호

---
+++ Object/src/main/java/com/example/oop/Invitation.java
@@ -0,0 +1,7 @@
+package com.example.oop;
+
+import java.time.LocalDateTime;
+
+public class Invitation {
+    private LocalDateTime when;
+}
---
+++ Object/src/main/java/com/example/oop/Ticket.java
@@ -0,0 +1,9 @@
+package com.example.oop;
+
+public class Ticket {
+    private Long fee;
+
+    public Long getFee(){
+        return fee;
+    }
+}
