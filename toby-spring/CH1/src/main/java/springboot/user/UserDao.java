458c48e84111cdf9d4635a7e9d775148465ab071
tovbskvb@daum.net <tovbskvb@daum.net>
2023-01-03 21:42:06 +0900

    [3.4.2] JdbcContext 분리해서 UserDao와 DI 구조를 위한 JdbcContext 및 xml 추가

--- toby-spring/CH1/src/main/java/springboot/user/UserDao.java
+++ toby-spring/CH1/src/main/java/springboot/user/UserDao.java
@@ -7,6 +7,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import springboot.dao.ConnectionMaker;
 import springboot.domain.User;
+import springboot.user.dao.JdbcContext;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -19,11 +20,21 @@ import java.util.ArrayList;
 // 리스트 1-6 독립된 SimpleConnectionMaker를 사용하게 만든 UserDao
 
 public class UserDao {
-     private ConnectionMaker connectionMaker;
-     private DataSource dataSource;
+
+    private ConnectionMaker connectionMaker;
+
+    private DataSource dataSource;
+
+    private JdbcContext jdbcContext;
+
+    public void setJdbcContext(JdbcContext jdbcContext){
+        this.jdbcContext = jdbcContext;
+    }
 
     public void setDataSource(DataSource dataSource) {
+
         this.dataSource = dataSource;
+
     }
 
 //    public UserDao(){
@@ -40,22 +51,17 @@ public class UserDao {
 //    }
 
     public void add(User user) throws SQLException{
+        this.jdbcContext.workWithStatementStrategy(new StatementStrategy() {
+            @Override
+            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+                PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+                ps.setString(1, user.getId());
+                ps.setString(2, user.getName());
+                ps.setString(3, user.getPassword());
+                return ps;
+            }
+        });
 
-        jdbcContextWithStatementStrategy(
-                new StatementStrategy() {
-                    @Override
-                    public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-                        PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
-                        ps.setString(1, user.getId());
-                        ps.setString(2, user.getName());
-                        ps.setString(3, user.getPassword());
-                        return ps;
-                    }
-                }
-        );
-
-//        StatementStrategy st = new AddStatement(user);
-//        jdbcContextWithStatementStrategy(st);
     }
 
     public User get(String id) throws SQLException, ClassNotFoundException {
@@ -87,15 +93,13 @@ public class UserDao {
     }
 
     public void deleteAll() throws SQLException{
-        jdbcContextWithStatementStrategy(new StatementStrategy() {
+        this.jdbcContext.workWithStatementStrategy(new StatementStrategy() {
             @Override
             public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
                 PreparedStatement ps = c.prepareStatement("delete from users");
                 return ps;
             }
         });
-//        StatementStrategy st = new DeleteAllStatement(); // 선정한 전략 클래스의 오브젝트 생성
-//        jdbcContextWithStatementStrategy(st);
     }
 
     public int getCount() throws SQLException{
@@ -137,31 +141,5 @@ public class UserDao {
     }
 
 
-    public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException{
-        Connection c = null;
-        PreparedStatement ps = null;
 
-        try {
-            c = dataSource.getConnection();
-            ps = stmt.makePreparedStatement(c);
-            ps.executeUpdate();
-        } catch (SQLException e){
-            throw e;
-        } finally {
-            if (ps != null){
-                try {
-                    ps.close();
-                } catch (SQLException e){
-
-                }
-            }
-            if (c != null){
-                try {
-                    c.close();
-                } catch (SQLException e){
-
-                }
-            }
-        }
-    }
 }
